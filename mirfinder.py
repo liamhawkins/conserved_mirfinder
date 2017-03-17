ed9acc5236d5006a89c65fd363f3021f1d54e1de
Liam Hawkins <liamhawkinscu@gmail.com>
2017-03-17 15:43:44 -0400

    Cleaned up output after mature vs potential stemloop blast

--- mirfinder.py
+++ mirfinder.py
@@ -4,11 +4,12 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
 
-QUERY_NAME = 'mmu-miR-24-1'
+QUERY_NAME = 'mmu-miR-15a'
 STEMLOOP_OUT_FILE = './output/stemloop-blast/stemloop-blast-' + QUERY_NAME + '.xml'
-STEMLOOP_QUERY_FILE = './queries/' + QUERY_NAME + '.fasta'
+STEMLOOP_QUERY_FILE = './queries/stemloop/' + QUERY_NAME + '.fasta'
 MATURE_QUERY_FILE = './queries/mature/' + QUERY_NAME + '.fasta'
 DB_FILES = './db/xen/xen'
+SPECIES_ABV = 'xen'
 E_VALUE_THRESH = 0.04
 
 
@@ -18,32 +19,24 @@ def blast_stemloops_vs_genome(query, db, out):
 
 
 def blast_mature_vs_potential_stemloops(query, subject, out):
-    blastn_cline = NcbiblastnCommandline(cmd='blastn', out=out, outfmt=5, query=query, subject=subject, word_size=10)
+    blastn_cline = NcbiblastnCommandline(cmd='blastn', out=out, outfmt=5, query=query, subject=subject, word_size=19)
     blastn_cline()
 
 blast_stemloops_vs_genome(STEMLOOP_QUERY_FILE, DB_FILES, STEMLOOP_OUT_FILE)
 
 result_handle = open(STEMLOOP_OUT_FILE)
 blast_records = NCBIXML.parse(result_handle)
-# blast_records = list(blast_records)
 
 for blast_record in blast_records:
     seq_list = []
     for alignment in blast_record.alignments:
         for hsp in alignment.hsps:
             if hsp.expect < E_VALUE_THRESH:
-                # print('****Alignment***')
-                # print('Sequence:', alignment.title)
-                # print('Length:', alignment.length)
-                # print('e value:', hsp.expect)
-                # print(hsp.query)
-                # print(hsp.match)
-                # print(hsp.sbjct)
-                # print('xen hp for', blast_record.query[0:-10] + ':', hsp.sbjct.replace('-', ''))
-                seq_list.append(SeqRecord(Seq(hsp.sbjct.replace('-','')), id=alignment.title))
-    phairpins_output_file = './output/potential-hairpins/' + blast_record.query + '-potential-hairpins.fasta'
-    SeqIO.write(seq_list, phairpins_output_file, 'fasta')
-    blast_mature_vs_potential_stemloops(phairpins_output_file, MATURE_QUERY_FILE, STEMLOOP_OUT_FILE)
+                seq_list.append(SeqRecord(Seq(hsp.sbjct.replace('-', '')), id=alignment.title))
+
+    pstemloops_output_file = './output/potential-stemloops/' + blast_record.query + '-potential-stemloops.fasta'
+    SeqIO.write(seq_list, pstemloops_output_file, 'fasta')
+    blast_mature_vs_potential_stemloops(pstemloops_output_file, MATURE_QUERY_FILE, STEMLOOP_OUT_FILE)
 
     mature_handle = open(STEMLOOP_OUT_FILE)
     mature_records = NCBIXML.parse(mature_handle)
@@ -51,9 +44,8 @@ for blast_record in blast_records:
     for mature_record in mature_records:
         for mature_alignment in mature_record.alignments:
             for mature_hsp in mature_alignment.hsps:
-                #if mature_hsp.align_length > (mature_alignment.length - 3):
-                print(mature_record.query)
-                print(mature_alignment.hit_def)
-                print(mature_hsp.query)
-                print(mature_hsp.match)
+                query_mir_name = mature_alignment.hit_def
+                query_mir_name = query_mir_name.split(' ')
+                print('Original miRNA:', mature_alignment.hit_def)
+                print('Possible conserved miRNA:', SPECIES_ABV + query_mir_name[0][3:])
                 print(mature_hsp.sbjct)
