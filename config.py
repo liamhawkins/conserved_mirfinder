eecf502fa5c0e9b598d99571c3a9e508fb3a4820
Liam Hawkins <liamhawkinscu@gmail.com>
2017-03-19 10:59:29 -0400

    Add min_mature_length to config file

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ mature_query_file = './queries/matures/' + query_name + '.fasta'
 csv_filename = './output/' + query_name + '_potential_sequences.csv'
 genome_db = './db/xen/xen'
 species_abreviation = 'xen'
+min_mature_length = 21
 e_value_threshold = 0.04
